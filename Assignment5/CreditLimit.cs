b62b7fe5710d931ea206c588189afec912568923
Bryant Wooters <jesusrox45@gmail.com>
2019-06-19 14:19:44 -0500

    App that lets you know your max credit limit

---
+++ Assignment5/CreditLimit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class CreditLimit
+    {
+        static void Main(string[] args)
+        {
+            int accountNum;
+            double begBalance, charge, credit, credLimit;
+            double newBalance;
+            
+
+            Console.Write("Enter the account number:");
+            accountNum = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the balance: $");
+            begBalance = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the charge: $");
+            charge = double.Parse(Console.ReadLine());
+
+            Console.Write("Credit: $");
+            credit = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Credit Limit: $");
+            credLimit = double.Parse(Console.ReadLine());
+
+
+            newBalance = (begBalance + charge) - credit;
+            Console.WriteLine("$" + newBalance);
+
+            if (newBalance > credLimit)
+            {
+                Console.WriteLine("Your credit has exceeded the credit limit.");
+            }
+            else if (newBalance < credLimit)
+            {
+                Console.WriteLine("Your credit has not exceeded the credit limit.");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Assignment5/Debug.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Debug
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            if (age >= 65)
+            {
+                Console.WriteLine("Age greater than or equal to 65.");
+            }
+            else
+            {
+                Console.WriteLine("Age is less than 65.");
+            }
+            int x = 1, total = 0;
+            while (x <= 10) 
+            {
+                total += x;
+                ++x;
+            }
+            while (x <= 100)
+            {
+                total += x;
+                ++x;
+            }
+            int y = 0;
+            while (y > 0)
+            {
+                Console.WriteLine(y);
+                ++y;
+            }
+            Console.ReadKey();
+        }
+    }
+}
